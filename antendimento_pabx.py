de2135a23de8d23b6733c8ffcfe31df9a37a26b4
Thiago Lima <51998758+ThiagoInterativa@users.noreply.github.com>
2026-03-30 18:02:10 -0300

    Update antendimento_pabx.py

--- antendimento_pabx.py
+++ antendimento_pabx.py
@@ -104,7 +104,7 @@ def pegar_status(session):
 def gerar_dashboard_html(agentes):
     # Mapeamento de status para cores do Bootstrap ou customizadas
     cores = {
-        "livre": "$green-400",
+        "livre": "$Success",
         "ocupado": "danger",
         "em pausa": "purple-700",  # classe customizada para roxo
         "offline": "secondary"     # exemplo de status extra
