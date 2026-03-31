dca2943f403101040c4547f320ce6d318a823f93
Thiago Lima <51998758+ThiagoInterativa@users.noreply.github.com>
2026-03-31 14:42:47 -0300

    Update antendimento_pabx.py

--- antendimento_pabx.py
+++ antendimento_pabx.py
@@ -156,8 +156,8 @@ def gerar_dashboard_html(agentes):
 
         # Escolhe o ícone e a cor do ícone
         if status == "livre": icone = f'<i class="bi bi-check-circle-fill text-{cor} me-1"></i>'
-        elif status == "tocando": icone = f'<i class="bi bi-telephone-fill text-{cor} me-1"></i>' 
-        elif status == "Tocando": icone = f'<i class="bi bi-telephone-fill text-{cor} me-1"></i>'    
+        elif status == "tocando": icone = f'<i class="bi bi-telephone-inbound-fill text-{cor} me-1"></i>' 
+        elif status == "Tocando": icone = f'<i class="bi bi-telephone-inbound-fill text-{cor} me-1"></i>'    
         elif status == "ocupado": icone = f'<i class="bi bi-x-circle-fill text-{cor} me-1"></i>'
         elif status == "em pausa": icone = f'<i class="bi bi-pause-circle-fill text-{cor} me-1"></i>'
         else: icone = f'<i class="bi bi-circle-fill text-{cor} me-1"></i>'
